8e50f4ff46ade8480be07f69a1e16e8440acd24d
Your Name <eleconte3@aol.com>
2024-12-18 16:56:43 +0000

    Initial commit with Vite React setup

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://pelerinagesdegap.fr/api'; // Remplacez par l'URL de votre backend
+const API_BASE_URL = 'https://resa.pelerinagesdegap.fr/api'; // Remplacez par l'URL de votre backend
 
 // Configuration par défaut d'axios
 const api = axios.create({
