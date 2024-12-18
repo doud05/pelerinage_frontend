4a7f6ea2a5f3b88f1d22d2c80c1a216b72dcaa75
doud05 <eleconte3@aol.com>
2024-12-18 20:02:18 +0100

    Update Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from '../components/Auth/LoginForm';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Connexion</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
