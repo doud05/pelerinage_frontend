f08fa62ede3d3b9bec6d4e533a4ea7aca744c645
doud05 <eleconte3@aol.com>
2024-12-18 20:02:42 +0100

    Update Register.jsx

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from '../components/Auth/RegisterForm';
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Inscription</h1>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
