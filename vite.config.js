e1bf67dd11f243f4555009ebd00cb5e0deecf694
doud05 <eleconte3@aol.com>
2024-12-22 19:17:30 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true, // Pour déboguer
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
-      },
-    },
   },
   define: {
     'process.env': process.env, // Inclut toutes les variables d'environnement
   },
-});
+});  
