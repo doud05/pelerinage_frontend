661a2cd5421d248168b88326bdacc87eaf5b4b72
doud05 <eleconte3@aol.com>
2024-12-22 16:31:08 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true, // Pour déboguer
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
   },
   define: {
     'process.env': process.env, // Inclut toutes les variables d'environnement
