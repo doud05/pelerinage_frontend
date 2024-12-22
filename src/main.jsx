1edfa39cbd63465e8bee343ad997f8d613d08e0a
doud05 <eleconte3@aol.com>
2024-12-22 19:20:13 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,55 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginRegister from './pages/LoginRegister';
-import DashboardPelerin from './pages/DashboardPelerin';
-import DashboardGestionnaire from './pages/DashboardGestionnaire';
-import DashboardAdmin from './pages/DashboardAdmin';
-import NotFound from './pages/NotFound';
-import PrivateRoute from './routes/PrivateRoute';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import AuthProvider from './context/AuthContext';
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* Page d'accueil (connexion ou inscription) */}
-        <Route path="/" element={<LoginRegister />} />
-        <Route path="/login" element={<LoginRegister />} />
-
-        {/* Tableau de bord pour les pèlerins */}
-        <Route
-          path="/dashboard/pelerin"
-          element={
-            <PrivateRoute allowedRoles={['pelerin']}>
-              <DashboardPelerin />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Tableau de bord pour les gestionnaires */}
-        <Route
-          path="/dashboard/gestionnaire"
-          element={
-            <PrivateRoute allowedRoles={['gestionnaire']}>
-              <DashboardGestionnaire />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Tableau de bord pour les administrateurs */}
-        <Route
-          path="/dashboard/admin"
-          element={
-            <PrivateRoute allowedRoles={['admin']}>
-              <DashboardAdmin />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Page non trouvée */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-};
-
-export default App; 
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+); 
