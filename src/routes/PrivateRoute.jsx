f32b9ba88d988c3d5239e9718c961ae9c2b353de
doud05 <eleconte3@aol.com>
2024-12-22 15:59:01 +0100

    Update PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -7,13 +7,16 @@ const PrivateRoute = ({ allowedRoles = [] }) => {
 
   // Si l'utilisateur n'est pas connecté, redirige vers la page de connexion
   if (!user) {
+    console.warn("Utilisateur non connecté.");
     return <Navigate to="/login" replace />;
   }
 
   // Si le rôle de l'utilisateur n'est pas autorisé, redirige vers l'accueil
   if (allowedRoles.length && !allowedRoles.includes(user.role)) {
+    console.warn(`Rôle non autorisé : ${user.role}`);
     return <Navigate to="/" replace />;
   }
+
 
   // Si tout va bien, rend le contenu protégé
   return <Outlet />;
