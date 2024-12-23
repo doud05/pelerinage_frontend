317cd6e469b214c72e566ef3e729cbfcc34f67d8
doud05 <eleconte3@aol.com>
2024-12-23 15:23:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import AuthProvider from './context/AuthContext';
+import AuthContextProvider from './context/AuthContextProvider';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
+    <AuthContextProvider>
       <App />
-    </AuthProvider>
+    </AuthContextProvider>
   </React.StrictMode>
-); 
+);
