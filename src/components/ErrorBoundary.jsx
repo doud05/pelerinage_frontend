279f6c357182e10e811266a13d1cd69965e1ae66
doud05 <eleconte3@aol.com>
2024-12-23 23:22:24 +0100

    Update ErrorBoundary.jsx

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -7,18 +7,18 @@ class ErrorBoundary extends React.Component {
   }
 
   static getDerivedStateFromError(error) {
+    console.error('Erreur capturée par ErrorBoundary :', error);
     return { hasError: true };
   }
 
   componentDidCatch(error, errorInfo) {
-    console.error('Erreur capturée par ErrorBoundary :', error, errorInfo);
+    console.error('Détails de l\'erreur capturée :', error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
       return <h1>Une erreur est survenue. Veuillez réessayer plus tard.</h1>;
     }
-
     return this.props.children;
   }
 }
