66a0dcaf5c269269b7e9eac0987e8038ebc6098a
doud05 <eleconte3@aol.com>
2024-12-23 15:47:59 +0100

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>404 - Page non trouvée</h1>
-      <p>Oups ! La page que vous recherchez n'existe pas.</p>
+      <p>Oups ! La page que vous recherchez n&apos;existe pas.</p>
       <Link to="/login" style={{ color: 'blue', textDecoration: 'underline' }}>
         Retour à la page de connexion
       </Link>
