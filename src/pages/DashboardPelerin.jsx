661f91b2dc726c179703fed9c056a07a3de05ee0
doud05 <eleconte3@aol.com>
2024-12-23 16:45:39 +0100

    Update DashboardPelerin.jsx

--- src/pages/DashboardPelerin.jsx
+++ src/pages/DashboardPelerin.jsx
@@ -1,4 +1,10 @@
+import React, { useEffect } from 'react';
+
 const DashboardPelerin = () => {
+  useEffect(() => {
+    console.log('Composant DashboardPelerin monté.');
+  }, []);
+
   return (
     <div>
       <h1>Tableau de bord : Pèlerin</h1>
