4c899feab7427a4cd8fc1cf13108943d94125c6c
doud05 <eleconte3@aol.com>
2024-12-24 10:14:25 +0100

    Update PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,18 +1,22 @@
 import { useContext } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContextProvider';
 import PropTypes from 'prop-types';
 
 const PrivateRoute = ({ allowedRoles = [] }) => {
   const { user } = useContext(AuthContext);
+  const location = useLocation();
 
   console.log('Utilisateur récupéré dans PrivateRoute avec Outlet :', user);
+  console.log('Route actuelle :', location.pathname);
 
+  // Vérifier si l'utilisateur est connecté
   if (!user) {
     console.warn('Utilisateur non connecté. Redirection vers /login.');
     return <Navigate to="/login" replace />;
   }
 
+  // Vérifier si l'utilisateur a un rôle autorisé
   if (allowedRoles.length && !allowedRoles.includes(user.role)) {
     console.warn(`Accès refusé : rôle ${user.role} non autorisé.`);
     return <Navigate to="/" replace />;
