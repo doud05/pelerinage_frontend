589ac061806a3649e5e4d81f729671e44a9a6b5e
doud05 <eleconte3@aol.com>
2024-12-24 09:39:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
-// Charge les variables d'environnement
+// Charge les variables d'environnement depuis le fichier .env
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Définit le port du serveur de développement
+    open: true, // Ouvre automatiquement le navigateur
+  },
   build: {
-    sourcemap: true, // Pour déboguer
+    sourcemap: true, // Active les cartes de source pour faciliter le débogage
   },
   define: {
     // Injecte explicitement les variables d'environnement
     'process.env': {
-      VITE_API_URL: process.env.VITE_API_URL,
+      VITE_API_URL: process.env.VITE_API_URL || 'http://localhost:5000', // URL API par défaut
     },
   },
 });
