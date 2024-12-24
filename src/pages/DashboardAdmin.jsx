5624dc01be40001857519d98deff02f028712b4d
doud05 <eleconte3@aol.com>
2024-12-24 10:14:45 +0100

    Update DashboardAdmin.jsx

--- src/pages/DashboardAdmin.jsx
+++ src/pages/DashboardAdmin.jsx
@@ -3,24 +3,14 @@ import React, { useEffect } from 'react';
 const DashboardAdmin = () => {
   useEffect(() => {
     console.log('Composant DashboardAdmin monté.');
-
-    return () => {
-      console.log('Composant DashboardAdmin démonté.');
-    };
   }, []);
 
-  try {
-    console.log('Rendu du composant DashboardAdmin.');
-    return (
-      <div>
-        <h1>Tableau de bord : Administrateur</h1>
-        <p>Gérez les utilisateurs et les rôles ici.</p>
-      </div>
-    );
-  } catch (error) {
-    console.error('Erreur lors du rendu de DashboardAdmin :', error);
-    return <div>Erreur lors du chargement du tableau de bord administrateur.</div>;
-  }
+  return (
+    <div>
+      <h1>Tableau de bord : Administrateur</h1>
+      <p>Gérez les utilisateurs et les rôles ici.</p>
+    </div>
+  );
 };
 
 export default DashboardAdmin;
