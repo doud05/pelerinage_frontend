f15e1e46b46723f1ab0f56080ee708fe4f05f54e
doud05 <eleconte3@aol.com>
2024-12-27 10:15:29 +0100

    Update ErrorBoundary.jsx

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -17,6 +17,7 @@ class ErrorBoundary extends React.Component {
 
   render() {
     if (this.state.hasError) {
+      console.error('Rendu du fallback dans ErrorBoundary.');
       return <h1>Une erreur est survenue. Veuillez réessayer plus tard.</h1>;
     }
     return this.props.children;
